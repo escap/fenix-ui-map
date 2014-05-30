2ac36a4ac0fe1c202f1b811ba9ac94ab7b31828c
vortex <simone.murzilli@fao.org>
2014-05-30 09:38:55 +0200

    refactor to latest folder

--- build/minify-fenix-map-js.sh
+++ build/minify-fenix-map-js.sh
@@ -8,12 +8,12 @@
 uglify-js/bin/uglifyjs --overwrite --output fenix-map-min.js --no-copyright --stats --lint ../src/FENIXMap.js ../src/core/Class.js ../src/core/Util.js ../src/core/hashmap.js ../src/core/RequestHandler.js ../src/core/UIUtils.js ../src/core/WMSUtils.js ../src/core/fullscreen.js ../src/map/config/DEPENDENCIES.js ../src/map/constants/*.js ../src/map/Map.js ../src/map/utils/LayerLegend.js ../src/map/controller/MapControllerDraggable.js ../src/map/utils/*.js  ../src/map/layer/*.js ../src/map/gui/*.js ../src/plugins/FMPopUp.js
 
 # move the library to the dist folder
-cp fenix-map-min.js ../dist/0.2/
+cp fenix-map-min.js ../dist/latest/
 
 # add a boolean to check if copy all or not
-cp -r ../src/plugins/ ../dist/0.2/
-cp -r ../dist/css/* ../dist/0.2/
-cp -r ../dist/I18N ../dist/0.2/
+cp -r ../src/plugins/ ../dist/latest/
+cp -r ../dist/css/* ../dist/latest/
+cp -r ../dist/I18N ../dist/latest/
 
 # remove file that is created
 rm fenix-map-min.js
