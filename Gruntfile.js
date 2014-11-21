6c168cf0d36a115b76cd7d5c58a3249e19f22c3e
stefanocudini <stefano.cudini@gmail.com>
2014-11-21 14:04:17 +0100

    GRUNTED

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,8 @@ grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-clean');
 grunt.loadNpmTasks('grunt-contrib-cssmin');
 grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-jsonlint');
 grunt.loadNpmTasks('grunt-contrib-watch');
 
 grunt.initConfig({
@@ -16,8 +18,7 @@ grunt.initConfig({
 		'/* \n'+
 		' * <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> \n'+
 		' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> \n'+
-		' * <%= pkg.author.email %> \n'+
-		' * <%= pkg.author.url %> \n'+
+		' * <%= pkg.author %> \n'+
 		' * \n'+
 		' * Licensed under the <%= pkg.license %> license. \n'+
 		' * \n'+
@@ -40,45 +41,104 @@ grunt.initConfig({
 			"-W044": true,	//ignore regexp
 			"-W033": true
 		},
-		files: ['src/*.js']
+		files: ['src/**/.js']
+	},
+	jsonlint: {
+		sample: {
+			src: [ 'i18n/*.json' ]
+		}
+	},	
+	copy: {
+		fenixmapconfig: {
+			nonull: true,
+			src: 'src/fenix-ui-map-config.js',
+			dest: 'dist/fenix-ui-map-config.js'
+		},
+		imageslayers: {
+			nonull: true,
+			expand: true,
+			//flatten: true,
+			cwd: "src/images/",
+			src: '**',
+			dest: "dist/images/"
+		},
+		i18n: {
+			nonull: true,
+			expand: true,
+			cwd: 'src/i18n/',
+			src: '*',
+			dest: 'dist/i18n/'
+		}		
 	},
 	concat: {
-		//TODO cut out SearchMarker
 		options: {
-			banner: '<%= meta.banner %>'
-		},
-		dist: {
-			files: {
-//				'dist/fenix-ui-map.src.js': ['src/fenix-ui-map.js'],			
-//				'dist/fenix-ui-map.src.css': ['src/fenix-ui-map.css']
+			banner: '<%= meta.banner %>',
+			separator: ';\n',
+			stripBanners: {
+				block: true
 			}
+		},
+		lib: {
+			src: [
+				'lib/jquery.min.js',
+				'lib/jquery.i18n.properties-min.js',
+				'lib/jquery-ui.custom.min.js',
+				'lib/jquery.hoverIntent.js',
+				'lib/leaflet.js',
+				'lib/leaflet.markercluster.js',
+				'lib/json2.js',
+				'lib/csvjson.min.1.0.js'
+			],
+			dest: 'dist/fenix-ui-map-lib.js'
+		},
+		fenixmap: {
+			src: [
+				'src/FENIXMap.js',
+				'src/core/Class.js',
+				'src/core/Util.js',
+				'src/core/hashmap.js',
+				'src/core/RequestHandler.js',
+				'src/core/UIUtils.js',
+				'src/core/WMSUtils.js',
+				'src/core/fullscreen.js',
+				'src/map/config/DEPENDENCIES.js',
+				'src/map/constants/*.js',
+				'src/map/Map.js',
+				'src/map/utils/LayerLegend.js',
+				'src/map/controller/MapControllerDraggable.js',
+				'src/map/utils/*.js ',
+				'src/map/layer/*.js',
+				'src/map/gui/*.js',
+				'src/plugins/FMPopUp.js'
+			],
+			dest: 'dist/fenix-ui-map.src.js'
 		}
 	},
 	uglify: {
 		options: {
 			banner: '<%= meta.banner %>'
 		},
-		dist: {
+		fenixmap: {
 			files: {
-//				'dist/fenix-ui-map.min.js': ['dist/fenix-ui-map.src.js']
+				'dist/fenix-ui-map.min.js': ['<%= concat.fenixmap.dest %>']
 			}
 		}
 	},
 	cssmin: {
-		combine: {
-			files: {
-//				'dist/fenix-ui-map.min.css': ['src/fenix-ui-map.css']
-			}
-		},
 		options: {
 			banner: '<%= meta.banner %>'
+		},
+		combine: {
+			src: [
+				'src/css/fenix-ui-leaflet.css',
+				'src/css/fenix-ui-map.css'				
+			],
+			dest: 'dist/fenix-ui-map.min.css'
 		},
 		minify: {
 			expand: true,
 			cwd: 'dist/',
-			files: {
-//				'dist/fenix-ui-map.min.css': ['src/fenix-ui-map.css']
-			}
+			src: '<%= cssmin.combine.dest %>'
 		}
 	},
 	watch: {
@@ -91,11 +151,13 @@ grunt.initConfig({
 });
 
 grunt.registerTask('default', [
+	//'jshint',
 	'clean',
-	'concat',	
+	'concat:fenixmap',
+	'uglify',	
 	'cssmin',
-	'jshint',
-	'uglify'
+	'jsonlint',	
+	'copy'
 ]);
 
 };
--- scripts/minify-fenix-map-js.sh
+++ scripts/minify-fenix-map-js.sh
@@ -10,14 +10,33 @@ DEMO_FOLDER=projects
 
 
 # TODO: use a variable for the files (or an external source)
-uglify-js/bin/uglifyjs --overwrite --output fenix-map-min.js --no-copyright --stats --lint ../src/FENIXMap.js ../src/core/Class.js ../src/core/Util.js ../src/core/hashmap.js ../src/core/RequestHandler.js ../src/core/UIUtils.js ../src/core/WMSUtils.js ../src/core/fullscreen.js ../src/map/config/DEPENDENCIES.js ../src/map/constants/*.js ../src/map/Map.js ../src/map/utils/LayerLegend.js ../src/map/controller/MapControllerDraggable.js ../src/map/utils/*.js  ../src/map/layer/*.js ../src/map/gui/*.js ../src/plugins/FMPopUp.js
+uglify-js/bin/uglifyjs --overwrite --output fenix-map-min.js --no-copyright --stats 
+
+--lint
+/src/FENIXMap.js
+/src/core/Class.js
+/src/core/Util.js
+/src/core/hashmap.js
+/src/core/RequestHandler.js
+/src/core/UIUtils.js
+/src/core/WMSUtils.js
+/src/core/fullscreen.js
+/src/map/config/DEPENDENCIES.js
+/src/map/constants/*.js
+/src/map/Map.js
+/src/map/utils/LayerLegend.js
+/src/map/controller/MapControllerDraggable.js
+/src/map/utils/*.js 
+/src/map/layer/*.js
+/src/map/gui/*.js
+/src/plugins/FMPopUp.js
 
 # move the library to the dist folder
 cp fenix-map-min.js $DISTRIBUTION_FOLDER
 
 # add a boolean to check if copy all or not
-cp -r ../src/plugins/ $DISTRIBUTION_FOLDER
-cp -r ../dist/css/* $DISTRIBUTION_FOLDER
+cp -r /src/plugins/ $DISTRIBUTION_FOLDER
+cp -r /dist/css/* $DISTRIBUTION_FOLDER
 cp -r ../dist/I18N $DISTRIBUTION_FOLDER
 cp -r ../vendors/ $DISTRIBUTION_FOLDER
 
