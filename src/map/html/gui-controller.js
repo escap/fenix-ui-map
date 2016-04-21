ea4e169bd6a0b1ddb6a586461e72aded5e14d04e
stefanocudini <stefano.cudini@gmail.com>
2016-04-21 16:41:46 +0200

    controller external fix css

--- src/map/html/gui-controller.js
+++ src/map/html/gui-controller.js
@@ -3,12 +3,13 @@ FM.guiController = {
     boxIcons: '<div id="REPLACE-controller-box-icons-container" class="fm-icon-box-background fm-controller-box-icons-container"></div>',
 
     boxMenu: '<div class="fm-box-zindex fm-icon-box-background fm-controller-box-icons-container fm-controller-box" id="REPLACE-controller-box">' +
-                '<div id="REPLACE-controller-box-content"></div>' +
+                '<div id="REPLACE-controller-box-content" class="clearfix"></div>' +
             '</div>',
 
-    baselayerIcon: '<div class="fm-box-zindex"><div class="fm-icon-sprite fm-baselayers" id="REPLACE-controller-baselayerIcon"><div></div>',
+    baselayerIcon:
+    '<div class="fm-box-zindex fx-toolbar-map-holder "><div class="fm-icon-sprite fm-baselayers" id="REPLACE-controller-baselayerIcon"><div></div>',
     baselayerBox:
-    '<div class="fm-box-zindex" id="REPLACE-controller-baselayer-box">' +
+    '<div class="fm-box-zindex fx-toolbar-map-holder " id="REPLACE-controller-baselayer-box">' +
         '<div id="REPLACE-controller-baselayer-title" class="fm-controller-box-title">Baselayers</div>' +
         '<div id="REPLACE-controller-baselayer-remove" class="fm-icon-close-panel-sprite fm-icon-close fm-icon-right"></div>' +
         '<div class="fm-standard-hr"></div>' +
@@ -16,25 +17,26 @@ FM.guiController = {
     '</div>',
 
     baselayer:
-    '<div id="REPLACE-controller-box-item" class="fm-box-zindex fm-controller-box-item-baselayer-content">' +
-        '<div id="REPLACE-controller-item">' +
-            '<div class="fm-controller-box-item-baselayer-image" id="REPLACE-controller-item-baselayer-image"></div>' +
-            '<div class="fm-controller-box-item-baselayer-text">' +
-                '<div>' +
-                    '<label class="fm-controller-box-item-baselayer-text fm-controller-item-title" id="REPLACE-controller-item-title">'+
-                        '<input id="REPLACE-controller-item-radio"  class="fm-checkbox-hide" type="radio" name="MAPID" value="REPLACE">'+
-                    '<label>' +
-                '</div>' +
-                '<div style="clear:both"></div>' +
-                '<div class="fm-opacity-slider-baselayers" id="REPLACE-controller-item-opacity" style="display:none"></div>' +
+    '<div id="REPLACE-controller-box-item" class="fm-box-zindex fx-toolbar-map-holder  fm-controller-box-item-baselayer-content">' +
+    '<div id="REPLACE-controller-item">' +
+        '<div class="fm-controller-box-item-baselayer-image" id="REPLACE-controller-item-baselayer-image"></div>' +
+        '<div class="fm-controller-box-item-baselayer-text">' +
+            '<div>' +
+                '<label class="fm-controller-box-item-baselayer-text fm-controller-item-title" id="REPLACE-controller-item-title">'+
+                    '<input id="REPLACE-controller-item-radio"  class="fm-checkbox-hide" type="radio" name="MAPID" value="REPLACE">'+
+                '<label>' +
             '</div>' +
+            '<div style="clear:both"></div>' +
+            '<div class="fm-opacity-slider-baselayers" id="REPLACE-controller-item-opacity" style="display:none"></div>' +
         '</div>' +
+    '</div>' +
     '</div>',
 
     overlayIcon:
-        "<div class='fm-box-zindex'><div class='fm-icon-sprite fm-overlays' id='REPLACE-controller-overlayIcon'></div></div>",
-    overlayBox:  
-    '<div class="fm-box-zindex" id="REPLACE-controller-overlay-box">' +
+    "<div class='fm-box-zindex fx-toolbar-map-holder '><div class='fm-icon-sprite fm-overlays' id='REPLACE-controller-overlayIcon'></div></div>",
+
+    overlayBox:
+    '<div class="fm-box-zindex fx-toolbar-map-holder " id="REPLACE-controller-overlay-box">' +
         '<div id="REPLACE-controller-overlay-title" class="fm-controller-box-title">Selected Layers</div>' +
         '<div id="REPLACE-controller-overlay-remove" class="fm-icon-close-panel-sprite fm-icon-close fm-icon-right"></div>' +
         '<div class="fm-standard-hr"></div>' +
@@ -42,7 +44,7 @@ FM.guiController = {
     '</div>',
 
     overlay:
-    '<div id="REPLACE-controller-item-box" class="fm-box-zindex fm-controller-box-item">' +
+    '<div id="REPLACE-controller-item-box" class="fm-box-zindex fx-toolbar-map-holder  fm-controller-box-item">' +
         '<div id="REPLACE-controller-item" class="fm-controller-box-header">' +
             '<div class="fm-controller-box-header-text">' +
                 '<div class="fm-controller-item-title" id="REPLACE-controller-item-title" ></div>' +
@@ -52,8 +54,8 @@ FM.guiController = {
                     '</div>' +
                     '<div style="clear:both"></div>' +
                     '<div class="fm-controller-box-icons">' +
-                         '<div class="fm-icon-enable" id="REPLACE-controller-item-enabledisable"></div>' +
-                        '<div  class="fm-opacity-slider" style="margin-right:10px;" id="REPLACE-controller-item-opacity"></div>' +
+                        '<div class="fm-icon-enable" id="REPLACE-controller-item-enabledisable"></div>' +
+                        '<div class="fm-opacity-slider" style="margin-right:10px;" id="REPLACE-controller-item-opacity"></div>' +
                     '</div>' +
                     '<div style="clear:both"></div>' +
                     '<div class="fm-controller-box-subicons" id="REPLACE-controller-item-subicons" style="display:none;">' +
@@ -69,15 +71,18 @@ FM.guiController = {
         '</div>' +
     '</div>',
 
-    wmsLoaderIcon: "<div class='fm-box-zindex'><div class='fm-icon-sprite fm-wmsloader' id='REPLACE-controller-wmsLoaderIcon'></div></div>",
-    wmsLoaderBox: '<div class="fm-box-zindex" id="REPLACE-controller-wmsLoader-box" style="display:none; min-height:300px;">' +
-                    '<div id="REPLACE-controller-wmsLoader-title" class="fm-controller-box-title">WMS Loader</div>' +
-                    '<div id="REPLACE-controller-wmsLoader-remove" class="fm-icon-close-panel-sprite fm-icon-close fm-icon-right"></div>' +
-                    '<div class="fm-standard-hr"></div>' +
-                    '<div class="clear:both"></div>' +
-                    '<div class="fm-WMSLoaderDropDown" id="REPLACE-controller-wmsLoader-dropdown"></div>' +
-                    '<div id="REPLACE-controller-wmsLoader-content" class="fm-controller-wmsLoader-content"></div>' +
-                 '</div>',
+    wmsLoaderIcon:
+    "<div class='fm-box-zindex fx-toolbar-map-holder '><div class='fm-icon-sprite fm-wmsloader' id='REPLACE-controller-wmsLoaderIcon'></div></div>",
+    wmsLoaderBox:
+    '<div class="fm-box-zindex fx-toolbar-map-holder " id="REPLACE-controller-wmsLoader-box" style="display:none; min-height:300px;">' +
+        '<div id="REPLACE-controller-wmsLoader-title" class="fm-controller-box-title">WMS Loader</div>' +
+        '<div id="REPLACE-controller-wmsLoader-remove" class="fm-icon-close-panel-sprite fm-icon-close fm-icon-right"></div>' +
+        '<div class="fm-standard-hr"></div>' +
+        '<div class="clear:both"></div>' +
+        '<div class="fm-WMSLoaderDropDown" id="REPLACE-controller-wmsLoader-dropdown"></div>' +
+        '<div id="REPLACE-controller-wmsLoader-content" class="fm-controller-wmsLoader-content"></div>' +
+     '</div>',
+
     wmsLoaderLayer: '<div id="REPLACE-WMSLayer-box" class="fm-WMSLayer-box">' +
                         '<div id="REPLACE-WMSLayer-icon" class="fm-controller-box-icon fm-icon-info"></div>' +
                         '<div id="REPLACE-WMSLayer-title" class="fm-WMSLayer-title"></div>' +
